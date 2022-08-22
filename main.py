80cbe9a562443d6bc058ce198c7157bbf5f7793b
perfect-liu <34927779+perfect-liu@users.noreply.github.com>
2022-08-22 15:33:43 +0800

    Update main.py

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ app_id = os.environ["APP_ID"]
 app_secret = os.environ["APP_SECRET"]
 
 user_id = os.environ["USER_ID"]
+user_id = os.environ["USER1_ID"]
 template_id = os.environ["TEMPLATE_ID"]
 
 
